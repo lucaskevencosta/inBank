d6c1a024d4bee3cb10213a0c2b1bb5ed061964ce
lucaskeven <lucaskevencosta@gmail.com>
2016-06-15 20:45:28 -0300

    Criação das classes "Cliente" e "Simulação".

--- inBank/src/entidade/Cliente.java
+++ inBank/src/entidade/Cliente.java
@@ -1,5 +1,47 @@
 package entidade;
 
 public class Cliente {
+	
+	private String nome;
+	
+	private String cpf;
+	
+	private Double salLiquido;
+	
+	private int idade;
+	
+	
+	
+	public String getNome() {
+		return nome;
+	}
 
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Double getSalLiquido() {
+		return salLiquido;
+	}
+
+	public void setSalLiquido(Double salLiquido) {
+		this.salLiquido = salLiquido;
+	}
+
+	public int getIdade() {
+		return idade;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
 }
--- inBank/src/entidade/Simulacao.java
+++ inBank/src/entidade/Simulacao.java
@@ -1,5 +1,26 @@
 package entidade;
 
 public class Simulacao {
+	
+	private Double valorParcela;
+	
+	private int qtdParcelas;
 
+	public Double getValorParcela() {
+		return valorParcela;
+	}
+
+	public void setValorParcela(Double valorParcela) {
+		this.valorParcela = valorParcela;
+	}
+
+	public int getQtdParcelas() {
+		return qtdParcelas;
+	}
+
+	public void setQtdParcelas(int qtdParcelas) {
+		this.qtdParcelas = qtdParcelas;
+	}
+	
+	
 }
